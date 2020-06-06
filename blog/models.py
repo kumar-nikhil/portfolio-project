6fee443d2a2d523ab8ff1722298e074437d22a40
nikhil kumar <nikhilkumar2092@gmail.com>
2020-06-06 20:51:23 +0530

    Added homepage for jobs

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateField(blank=True, null=True, default=None)
-    description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='image_blog/', blank=True)
+    pub_date = models.DateTimeField(blank=True, null=True, default=None)
+    description = models.TextField(max_length=255)
+    image = models.ImageField(upload_to='images/', blank=True)
--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)
-    summary = models.CharField(max_length=255)
+    summary = models.TextField(max_length=255)
 
