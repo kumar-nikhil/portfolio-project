8a3a4d2f49a4018974d3f7d6390ce0dc01c215b6
nikhil kumar <nikhilkumar2092@gmail.com>
2020-06-06 19:59:55 +0530

    Added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateField(blank=True, null=True, default=None)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='image_blog/', blank=True)
--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True)
     summary = models.CharField(max_length=255)
 
