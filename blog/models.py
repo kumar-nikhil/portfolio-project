f26636e891ece95df62cbb9aa14dc58adef04a6b
nikhil kumar <nikhilkumar2092@gmail.com>
2020-06-07 17:05:04 +0530

    Handling for individual blog url

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(blank=True, null=True, default=None)
     description = models.TextField(max_length=255)
     image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.description[:50]
+
+    def date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/', blank=True)
+    title = models.CharField(max_length=100, default='title')
     summary = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
